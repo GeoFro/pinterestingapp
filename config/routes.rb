2662622cac107d32052ee21f29ea64962e12e012
George Frost <u5370263@anu.edu.au>
2014-06-29 13:20:37 +1000

    Added new devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "pages#home"
   get "about" => "pages#about" # Creates about_path 
   # ^Above here we have made a route for the new about page. 
