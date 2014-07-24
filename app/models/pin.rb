473ee218cec53978ea56c5e95fb3cf974d203158
GeoFro <u5370263@anu.edu.au>
2014-07-23 18:23:32 -0700

    Added names for user + other stuff

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,7 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://www.keley-consulting.com/assets/imageMissing-eb1c8bc863046c027f472df8ad09d7f2.jpg"
 
 validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
+  validates :image, presence: true
+  
 end
